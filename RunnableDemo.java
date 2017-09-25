8ad7b700954dbd38a458a566dc9f924aadfa954a
Justin Kim <jjkim928@gmail.com>
2017-09-25 17:40:40 -0400

    added anon class and lambda

--- RunnableDemo.java
+++ RunnableDemo.java
@@ -4,15 +4,20 @@ public class RunnableDemo {
 
         // !! - Write an anonymous Runnable class here
         //It can just print out "Anonymous run" or similar
-     //   Runnable anonRun = // new Runnable() here
+        Runnable anonRun = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("This was created using an Anonymous Runnable class.");
+            }
+        };
 
-      //  anonRun.run();
+        anonRun.run();
 
         // !! - Write a lambda function here
         //It can also print out something like "Lambda run"
-      //  Runnable lambdaRun = // lambda run here
+        Runnable lambdaRun = () -> System.out.println("This was created using a lambda function.");
 
-      //  lambdaRun.run();
+        lambdaRun.run();
 
     }
 }
